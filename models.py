0b29bf02b2bec188413329629bbb5dd679a1158b
jamayabarreto <jamayabarreto@gmail.com>
2025-02-18 00:18:34 -0500

    Updated app.py and added requirements.txt

--- models.py
+++ models.py
@@ -8,16 +8,6 @@ class MagicalGirl(db.Model):
     age = db.Column(db.Integer, nullable=False)
     city = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(100), nullable=False)
-    from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class MagicalGirl(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    city = db.Column(db.String(100), nullable=False)
-    status = db.Column(db.String(100), nullable=False)
     contract_date = db.Column(db.Date, nullable=False)
     race = db.Column(db.String(100), nullable=False)
     photo_url = db.Column(db.String(200), nullable=True)  # Nuevo campo para la URL de la foto
